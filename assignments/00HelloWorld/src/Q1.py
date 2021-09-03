188e12c73e8a10289c06a7f823790b3bd385e2b4
AlejandroL12 <A01740869@itesm.mx>
2021-09-03 14:17:36 +0000

    comp

---
+++ assignments/00HelloWorld/src/Q1.py
@@ -0,0 +1,8 @@
+def main():
+    #escribe tu código abajo de esta línea
+    cont=1
+    while (cont < 5 ):
+        print (cont) 
+        cont= 1+cont
+if __name__=='__main__':
+    main()
---
+++ assignments/00HelloWorld/src/Q2.py
@@ -0,0 +1,14 @@
+def main():
+    #escribe tu código abajo de esta línea
+
+    for cont in range(3,8,2):
+        print (cont)
+
+
+    conta=3
+    while (conta<8):
+        print (conta)
+        conta=conta+2
+
+if __name__=='__main__':
+    main()
